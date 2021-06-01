7c418463f94e6e6c6d6ba214c80d1e99c8ec5bd4
audeo23 <ludovic.vogel@gmail.com>
2021-06-01 19:17:26 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:audeo23/vue-pos.git master:gh-pages
+git push -f git@github.com:audeo23/vue-pos.git main:gh-pages
 
 cd -
